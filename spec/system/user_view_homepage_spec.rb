a9657586fd90fde0c5c42ad28c19b888b9617618
mbellucio <=>
2024-04-05 15:08:13 -0300

    User sees warehouses in the home page

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -10,4 +10,34 @@ describe "User acess home page and" do
     #assert
     expect(page).to have_content("Warehouses & Inventory")
   end
+
+  it "sees all registered warehouses" do
+    #arrange
+    Warehouse.create(name: "Rio", code: "SDU", city: "Rio de Janeiro", area: 60_000)
+    Warehouse.create(name: "Maceio", code: "MCZ", city: "Maceio", area: 50_000)
+
+    #act
+    visit("/")
+
+    #assert
+    expect(page).not_to have_content("There are no registered warehouses")
+    expect(page).to have_content("Rio")
+    expect(page).to have_content("Código: SDU")
+    expect(page).to have_content("Cidade: Rio de Janeiro")
+    expect(page).to have_content("60000 m2")
+
+    expect(page).to have_content("Maceio")
+    expect(page).to have_content("Código: MCZ")
+    expect(page).to have_content("Cidade: Maceio")
+    expect(page).to have_content("50000 m2")
+  end
+
+  it "there is no registered warehouses" do
+    #arrange
+
+    #act
+    visit("/")
+    #assert
+    expect(page).to have_content("There are no registered warehouses")
+  end
 end
