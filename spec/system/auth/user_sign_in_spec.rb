94f697ffbfb2c9a5e270f6882710ce3cac8efed2
mbellucio <=>
2024-04-15 13:58:08 -0300

    User sign up, sign in and sign out

---
+++ spec/system/auth/user_sign_in_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+describe "User authenticates" do
+  it "with success" do
+    #arrange
+    User.create!(email: "admin@gmail.com", password: "flamengo2019")
+    #act
+    visit root_path
+    click_on "Login"
+    within("form") do
+      fill_in "Email", with: "admin@gmail.com"
+      fill_in "Password",	with: "flamengo2019"
+      click_on "Login"
+    end
+    #assert
+    within("nav") do
+      expect(page).to have_button "Logout"
+      expect(page).not_to have_link "Login"
+      expect(page).to have_content "admin@gmail.com"
+    end
+    expect(page).to have_content "Signed in successfully."
+  end
+
+  it "and then logout" do
+    #arrange
+    User.create!(email: "admin@gmail.com", password: "flamengo2019")
+    #act
+    visit root_path
+    click_on "Login"
+    within("form") do
+      fill_in "Email", with: "admin@gmail.com"
+      fill_in "Password",	with: "flamengo2019"
+      click_on "Login"
+    end
+    click_on "Logout"
+    #assert
+    expect(page).to have_content "Signed out successfully."
+    expect(page).to have_content "Login"
+    expect(page).not_to have_content "Logout"
+    expect(page).not_to have_content "admin@gmail.com"
+  end
+end
---
+++ spec/system/auth/user_sign_up_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+describe "User signs up and authenticates" do
+  it "with success" do
+    #arrange
+    #act
+    visit root_path
+    click_on "Login"
+    click_on "Sign up"
+    within("form") do
+      fill_in "Name", with: "Ademiro"
+      fill_in "Email", with: "admin@gmail.com"
+      fill_in "Password", with: "flamengo2019"
+      fill_in "Password confirmation", with: "flamengo2019"
+      click_on "Sign up"
+    end
+    #assert
+    expect(page).to have_content "admin@gmail.com"
+    expect(page).to have_button "Logout"
+    expect(page).to have_content "Welcome! You have signed up successfully."
+    user = User.last
+    expect(user.name).to eq "Ademiro"
+  end
+end
